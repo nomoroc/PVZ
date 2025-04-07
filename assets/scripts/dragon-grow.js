bd04c1105fec1f2621b4f4c5f19ff221ff53a144
nomoroc <yuriydonskoi@gmail.com>
2025-04-07 22:34:49 +0300

    пытаюсь исправить баг со сменой спрайтов

--- assets/scripts/dragon-grow.js
+++ assets/scripts/dragon-grow.js
@@ -1,5 +1,6 @@
 const dragon = document.querySelector('.Dragon') // используем lowercase
 const now = document.querySelector('.now')
+const future = document.querySelector('.future')
 const clicksscore = document.querySelector('.clicks-score')
 const clicksscore2 = document.querySelector('.clicks-score2')
 
@@ -25,6 +26,7 @@ let clickHandler
 
 function setupDragon() {
 	let purpose = puproses.one
+	let fut = dragonSrc.two
 	const key = 'clicks'
 	let clicks = parseInt(localStorage.getItem(key)) || 0
 	clicksscore.innerText = clicks
@@ -33,28 +35,33 @@ function setupDragon() {
 
 		// Измененный порядок условий (от большего к меньшему)
 		let newSrc = dragonSrc.one
-		if (clicks >= 1000) {
+		if (clicks >= puproses.six) {
 			newSrc = dragonSrc.six
-		} else if (clicks >= 700) {
+		} else if (clicks >= puproses.four) {
 			newSrc = dragonSrc.five
 			purpose = puproses.five
-		} else if (clicks >= 350) {
+			fut = dragonSrc.six
+		} else if (clicks >= puproses.three) {
 			newSrc = dragonSrc.four
 			purpose = puproses.four
-		} else if (clicks >= 150) {
+			fut = dragonSrc.five
+		} else if (clicks >= puproses.two) {
 			newSrc = dragonSrc.three
 			purpose = puproses.three
-		} else if (clicks >= 50) {
+			fut = dragonSrc.four
+		} else if (clicks >= puproses.one) {
 			newSrc = dragonSrc.two
 			purpose = puproses.two
-		} else if (clicks < 50) {
+			fut = dragonSrc.three
+		} else if (clicks < puproses.one) {
 			newSrc = dragonSrc.one
 			purpose = puproses.one
+			fut = dragonSrc.two
 		}
 
 		clicksscore2.innerText = puproses - clicks
-		const img = new Image()
-		img.src = newSrc
+		new Image(now).src = newSrc
+		new Image(future).src = fut
 
 		localStorage.setItem(key, clicks.toString())
 		console.log('Текущее количество кликов:', clicks)
